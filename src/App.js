c2b184a1dd90b13798aff39d97cf4d76b9b64ab9
= <=>
2024-07-06 10:23:31 +0530

    hosting on github

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import './App.css';
-import Home from './components/home/Home';
-import NewsComponent from './components/news/NewsComponent';
+import "./App.css";
+import Home from "./components/home/Home";
+import NavBar from "./components/navbar/NavBar";
+import NewsComponent from "./components/news/NewsComponent";
+import Video from "./components/video/Video";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-      <Home/>
-      <NewsComponent/>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/video" element={<Video />} />
+          <Route path="/news" element={<NewsComponent />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
